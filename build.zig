eba42b7d21c9186270add72b520a3510ec3e506b
hazeycode <22148308+hazeycode@users.noreply.github.com>
2023-02-07 10:56:29 +0000

    build.zig: upgrade to latest Zig

--- build.zig
+++ build.zig
@@ -2,16 +2,20 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("xml", "Xml.zig");
-    lib.setTarget(target);
-    lib.setBuildMode(mode);
+    const lib = b.addStaticLibrary(.{
+        .name = "xml",
+        .root_source_file = .{ .path = "Xml.zig" },
+        .target = target,
+        .optimize = mode,
+    });
     lib.install();
 
-    const main_tests = b.addTest("Xml.zig");
-    main_tests.setTarget(target);
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "Xml.zig" },
+        .optimize = mode,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
